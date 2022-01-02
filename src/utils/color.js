a9251e6855149f7e97e4b6e28326b211cead2ea4
Brian Vaughn <bvaughn@fb.com>
2022-01-02 14:09:29 -0500

    Make string-to-color predictable, regardless of task editing order. This ensures shared URLs always match the original.

--- src/utils/color.js
+++ src/utils/color.js
@@ -36,49 +36,42 @@ export function getContrastRatio(colorA, colorB) {
   );
 }
 
-// TODO Make this array longer; use incoming string to determine array index
-// this way shared plans will always have consistent colors even if their in-memory editing history is different.
-
 let colorIndex = 0;
 const colors = [
-  "#363852",
-  "#726D81",
-  "#B29DA0",
-  "#DECBC6",
-  "#4A4E69",
-  "#9A8C98",
-  "#C9ADA7",
+  "#2D2327",
+  "#392D39",
+  "#45364B",
+  "#543E5B",
+  "#62466B",
   "#22223B",
-  "#F2E9E4",
+  "#363852",
+  "#4A4E69",
+  "#726D81",
+  "#9A8C98",
+  "#B29DA0",
+  "#C9ADA7",
+  "#DECBC6",
 ];
 const stringToColorMap = new Map();
 
 export function getColorForString(string) {
+  if (!string) {
+    return "#000000";
+  }
+
   string = string.toLowerCase();
 
   if (!stringToColorMap.has(string)) {
-    // Use pretty (hard-coded) colors for the first few tasks.
-    if (colorIndex < colors.length) {
-      const color = colors[colorIndex % colors.length];
-      colorIndex++;
-
-      stringToColorMap.set(string, color);
-    } else {
-      // Fall back to randomly generated colors if we run out.
-      // https://gist.github.com/0x263b/2bdd90886c2036a1ad5bcf06d6e6fb37
-      let hash = 0;
-      if (string.length === 0) return "#000000";
-      for (var i = 0; i < string.length; i++) {
-        hash = string.charCodeAt(i) + ((hash << 5) - hash);
-        hash = hash & hash;
-      }
-      let color = "#";
-      for (let i = 0; i < 3; i++) {
-        let value = (hash >> (i * 8)) & 255;
-        color += ("00" + value.toString(16)).substr(-2);
-      }
-      stringToColorMap.set(string, color);
+    let hash = 0;
+    for (var charIndex = 0; charIndex < string.length; charIndex++) {
+      hash = string.charCodeAt(charIndex) + ((hash << 5) - hash);
+      hash = hash & hash;
     }
+
+    const colorIndex = Math.abs(hash % colors.length);
+    const color = colors[colorIndex];
+
+    stringToColorMap.set(string, color);
   }
 
   return stringToColorMap.get(string);
