6ab6145b5bcc013e4442d8b598123bba75965f46
Brian Vaughn <bvaughn@fb.com>
2022-01-11 10:29:52 -0500

    Fixed broken twitter:url meta tag

--- components/OgMeta.js
+++ components/OgMeta.js
@@ -8,23 +8,23 @@ export default function OgMeta({
   title = "Planner JS",
   description = "Lightweight, interactive planner. Visualize tasks using an HTML canvas.",
 }) {
-  const baseUrl = getBaseURL();
+  const baseURL = getBaseURL();
 
   return (
     <Head>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={`${baseUrl}${url}`} />
+      <meta property="og:url" content={`${baseURL}${url}`} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`${baseUrl}${imageUrl}`} />
+      <meta property="og:image" content={`${baseURL}${imageUrl}`} />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={`{baseURL}${url}`} />
+      <meta property="twitter:url" content={`${baseURL}${url}`} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={`${baseUrl}${imageUrl}`} />
+      <meta property="twitter:image" content={`${baseURL}${imageUrl}`} />
     </Head>
   );
 }
